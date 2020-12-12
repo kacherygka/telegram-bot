a041dd841c8ffcfd18518e3c71d6e3b140ae4557
kacherygka <70934020+kacherygka@users.noreply.github.com>
2020-12-12 21:19:01 +0300

    Add files via upload

---
+++ valbot/utils.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException('Невозможно перевести одинаковые валюты.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')    
+        #https://api.exchangeratesapi.io/latest?base=
+        r = requests.get(f'https://api.exchangeratesapi.io/latest?symbols={quote_ticker},{base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
+
+
+        
